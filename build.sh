3f9952bed5797c62fd9cdf3582c1e2108559857a
Amir Raminfar <findamir@gmail.com>
2010-12-11 20:51:31 -0500

    adding run.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ cp -v `find application/target/lib/ -type f -not -name "*tool-1.0.jar"` bin/lib/
 cp -v `find application/target/lib/ -type f -name "*tool-1.0.jar"` bin/tools/
 cp -v application/target/application-1.0.jar bin/lib/
 cp -rv application/src/main/resources/lucene bin/
+echo "java -cp .:lib/*:tools/* edu.gwu.raminfar.iauthor.Main" > bin/run.sh
+chmod 755 bin/run.sh
 
