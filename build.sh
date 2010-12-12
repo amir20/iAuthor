5d5bff74a21a0db5c0f62de470fd56ac99635d1d
Amir Raminfar <findamir@gmail.com>
2010-12-12 16:15:06 -0500

    Updating build file

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-rm -rf bin
-mkdir -p bin/lib
-mkdir -p bin/tools
+rm -rf build
+mkdir -p build/lib
+mkdir -p build/tools
 mvn -Dmaven.test.skip=true clean package
-cp -v `find application/target/lib/ -type f -not -name "*tool-1.0.jar"` bin/lib/
-cp -v `find application/target/lib/ -type f -name "*tool-1.0.jar"` bin/tools/
-cp -v application/target/application-1.0.jar bin/lib/
-cp -rv application/src/main/resources/lucene bin/
-echo "java -cp .:lib/*:tools/* edu.gwu.raminfar.iauthor.Main" > bin/run.sh
-chmod 755 bin/run.sh
+cp -v `find application/target/lib/ -type f -not -name "*tool-1.0.jar"` build/lib/
+cp -v `find application/target/lib/ -type f -name "*tool-1.0.jar"` build/tools/
+cp -v application/target/application-1.0.jar build/lib/
+cp -rv application/src/main/resources/lucene build/
+cp -v application/src/main/resources/images/icon.png build/
+echo "java -Xdock:name="iAuthor" -Xdock:icon=icon.png -cp .:lib/*:tools/* edu.gwu.raminfar.iauthor.Main" > build/run.sh
+chmod 755 build/run.sh
 
