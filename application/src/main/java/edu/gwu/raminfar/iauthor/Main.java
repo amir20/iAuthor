0efe6e40efdc937fbf30ef42ba3d3326cc44f5cb
Amir Raminfar <amir.raminfar@opower.com>
2011-05-10 18:35:47 -0400

    Adding launch4j settings and binary

--- application/src/main/java/edu/gwu/raminfar/iauthor/Main.java
+++ application/src/main/java/edu/gwu/raminfar/iauthor/Main.java
@@ -9,11 +9,10 @@ import javax.swing.*;
  * Date: Sep 25, 2010
  */
 public class Main {
-    public static boolean IS_MAC;
-    public static String APP_NAME = "iAuthor";
+    public final static boolean IS_MAC = System.getProperty("os.name").toLowerCase().startsWith("mac os x");
+    public final static String APP_NAME = "iAuthor";
 
     public static void main(String... args) throws ClassNotFoundException, UnsupportedLookAndFeelException, IllegalAccessException, InstantiationException {
-        IS_MAC = System.getProperty("os.name").toLowerCase().startsWith("mac os x");
         if (IS_MAC) {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
             System.setProperty("com.apple.mrj.application.apple.menu.about.name", APP_NAME);
