443c3856f55453ce9c7d75ada328d4dc863b3e74
Amir Raminfar <amir.raminfar@opower.com>
2011-05-11 12:08:03 -0400

    Updating build file to have exe copied

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp -v `find application/target/lib/ -type f -name "*tool-1.0.jar"` build/tools/
 cp -v application/target/application-1.0.jar build/lib/
 cp -rv application/src/main/resources/lucene build/
 cp -v application/src/main/resources/images/icon.png build/
+cp -v application/windows/*.exe build/
 echo "java -Xdock:name="iAuthor" -Xdock:icon=icon.png -cp .:lib/*:tools/* edu.gwu.raminfar.iauthor.Main" > build/run.sh
 chmod 755 build/run.sh
 
